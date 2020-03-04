efa63694501317689d3a1c98db22ab47e28dc840
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-04 22:33:34 +0800

    .

--- app/src/main/java/com/swchen1217/fios/WelcomeActivity.java
+++ app/src/main/java/com/swchen1217/fios/WelcomeActivity.java
@@ -17,7 +17,7 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wolcome);
+        setContentView(R.layout.activity_welcome);
 
         getSupportActionBar().hide();
         logo = findViewById(R.id.imageView);
