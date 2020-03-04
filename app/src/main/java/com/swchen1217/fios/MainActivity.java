52a9e5b5cb10dbcc6bfdfcebafd873b55c6cce70
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-04 19:38:10 +0800

    new

---
+++ app/src/main/java/com/swchen1217/fios/MainActivity.java
@@ -0,0 +1,14 @@
+package com.swchen1217.fios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
